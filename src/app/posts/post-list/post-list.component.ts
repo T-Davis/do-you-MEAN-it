707a2b62eb0cccd5aea27f7651dc947ca885e20c
Trevor Davis <tzendou@gmail.com>
2020-04-23 15:38:13 -0700

    Subscribe to PostsService observable

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,16 +1,28 @@
-import {Component, Input} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Post} from '../post';
+import {PostsService} from '../posts.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
-  @Input()
-  posts: Post[] = [
-    // {title: 'first post', content: 'first post content'},
-    // {title: 'second post', content: 'second post content'},
-    // {title: 'third post', content: 'third post content'}
-  ];
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSub = Subscription.EMPTY;
+
+  constructor(public postsService: PostsService) {
+  }
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostsUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
