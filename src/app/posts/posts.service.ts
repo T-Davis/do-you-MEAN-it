1ddf1aedfc43b3d8b7134318a1861075cada169b
Trevor Davis <tzendou@gmail.com>
2020-04-23 15:12:19 -0700

    Make Post observable

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -5,13 +5,18 @@ import {Subject} from 'rxjs';
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdate = new Subject<Post[]>();
 
   getPosts() {
     return [...this.posts];
   }
 
+  getPostsUpdateListener() {
+    return this.postsUpdate.asObservable();
+  }
+
   addPost(title: string, content: string) {
-    const post: Post = {title, content};
-    this.posts.push(post);
+    this.posts.push({title, content});
+    this.postsUpdate.next([...this.posts]);
   }
 }
