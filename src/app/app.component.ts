61191f0938dbe65b80dfc0a69d43549fa576812e
Trevor Davis <tzendou@gmail.com>
2020-04-23 15:14:49 -0700

    Remove event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import {Post} from './posts/post';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +7,4 @@ import {Post} from './posts/post';
 })
 export class AppComponent {
   title = 'do-you-MEAN-it';
-  storedPosts: Post[] = [];
-
-  onPostAdded(post) {
-    this.storedPosts.push(post);
-  }
 }
