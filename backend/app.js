c0d3f73b5d390d5f7c045a699427dd5973a04031
Trevor Davis <tzendou@gmail.com>
2020-04-28 23:02:57 -0700

    Add /api/posts path

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-  res.send('Hello, World!');
+app.use("/api/posts", (req, res) => {
+  const posts = [
+    {
+      id: "123",
+      title: "first post",
+      content: "first post content"
+    },
+    {
+      id: "245",
+      title: "second post",
+      content: "second post content"
+    },
+  ];
+  res.status(200).json({
+    message: "posts fetched successfully",
+    posts: posts
+  });
 })
 
 module.exports = app;
