3239eb3426923a6d864a75d0028e9b1160984325
Trevor Davis <tzendou@gmail.com>
2020-04-29 11:36:45 -0700

    Update addPost to send posts to server

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -7,12 +7,13 @@ import {HttpClient} from '@angular/common/http';
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdate = new Subject<Post[]>();
+  private url = 'http://localhost:3000/api/posts';
 
   constructor(private http: HttpClient) {
   }
 
   getPosts() {
-    this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
+    this.http.get<{ message: string, posts: Post[] }>(this.url)
       .subscribe((postsData) => {
         this.posts = postsData.posts;
         this.postsUpdate.next([...this.posts]);
@@ -24,7 +25,11 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    this.posts.push({id: null, title, content});
-    this.postsUpdate.next([...this.posts]);
+    const post: Post = {id: null, title, content};
+    this.http.post<{ message: string }>(this.url, post).subscribe(responseData => {
+      console.log(responseData.message);
+      this.posts.push({id: null, title, content});
+      this.postsUpdate.next([...this.posts]);
+    });
   }
 }
