1855fe32dee150fd26f41e148e8df04ac6206a6b
Trevor Davis <tzendou@gmail.com>
2020-05-01 16:38:52 -0700

    Update GET request to /api/posts to fetch data from db

--- backend/app.js
+++ backend/app.js
@@ -43,21 +43,11 @@ app.post("/api/posts", (req, res) => {
 });
 
 app.get("/api/posts", (req, res) => {
-  const posts = [
-    {
-      id: "123",
-      title: "first post",
-      content: "first post content"
-    },
-    {
-      id: "245",
-      title: "second post",
-      content: "second post content"
-    },
-  ];
-  res.status(200).json({
-    message: "posts fetched successfully",
-    posts: posts
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: "posts fetched successfully",
+      posts: documents
+    });
   });
 });
 
