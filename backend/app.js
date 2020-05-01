35dc6fa0d90526812a33e59af8707f45d82a9b0f
Trevor Davis <tzendou@gmail.com>
2020-05-01 14:12:08 -0700

    Update /api/posts to send post requests to db

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.post("/api/posts", (req, res) => {
     title: req.body.title,
     content: req.body.content
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: "Post added successfully"
   });
