e887068b5cf6689023ea1f49f8d1b729248e0738
Trevor Davis <tzendou@gmail.com>
2020-05-04 18:51:44 -0700

    Move routes to routes/posts.js

---
+++ backend/routes/posts.js
@@ -0,0 +1,62 @@
+const express = require("express");
+
+const Post = require("../models/post");
+
+const router = express.Router();
+
+router.post("", (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added",
+      postId: createdPost._id
+    });
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({_id: req.params.id}, post).then(() => {
+    res.status(200).json({
+      message: "Post updated"
+    });
+  });
+});
+
+router.get("", (req, res) => {
+  Post.find().then(posts => {
+    res.status(200).json({
+      message: "Posts fetched",
+      posts: posts
+    });
+  });
+});
+
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({
+        message: "Post not found"
+      });
+    }
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  Post.deleteOne({_id: req.params.id}).then(() => {
+    res.status(200).json({
+      message: "Post deleted"
+    });
+  });
+});
+
+module.exports = router;
