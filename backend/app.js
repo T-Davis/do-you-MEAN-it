f6ee2d20bdd50098a1b541a8a6dd4eebc35f7be5
Trevor Davis <tzendou@gmail.com>
2020-05-04 18:23:40 -0700

    Add GET method findById()

--- backend/app.js
+++ backend/app.js
@@ -59,18 +59,32 @@ app.put("/api/posts/:id", (req, res) => {
 })
 
 app.get("/api/posts", (req, res) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({
+        message: "Post not found"
+      });
+    }
+  });
+});
+
+app.get("/api/posts/:id", (req, res) => {
   Post.find().then(documents => {
     res.status(200).json({
-      message: "posts fetched successfully",
+      message: "Posts fetched successfully",
       posts: documents
     });
   });
 });
 
-app.delete("/api/posts:id", (req, res) => {
+app.delete("/api/posts/:id", (req, res) => {
   Post.deleteOne({_id: req.params.id}).then(result => {
     console.log(result);
-    res.status(200).json({message: "Post deleted"});
+    res.status(200).json({
+      message: "Post deleted successfully"
+    });
   });
 });
 
