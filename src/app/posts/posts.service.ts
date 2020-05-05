783c11cb184cdfa9b4222682486b1f961057c820
Trevor Davis <tzendou@gmail.com>
2020-05-04 20:34:44 -0700

    Add getPost() to get single post by id

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,8 +1,7 @@
-import {Post} from './post';
 import {Injectable} from '@angular/core';
-import {Subject} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {Post} from './post';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -14,16 +13,18 @@ export class PostsService {
   }
 
   getPosts() {
-    this.http.get<{ message: string, posts: any }>(this.url)
-      .pipe(map((postData) => {
-        return postData.posts.map(post => {
-          return {
-            id: post._id,
-            title: post.title,
-            content: post.content
-          };
-        });
-      }))
+    this.http
+      .get<{ message: string, posts: any }>(this.url)
+      .pipe(
+        map((postData) => {
+          return postData.posts.map(post => {
+            return {
+              id: post._id,
+              title: post.title,
+              content: post.content
+            };
+          });
+        }))
       .subscribe((transformedPosts) => {
         this.posts = transformedPosts;
         this.postsUpdate.next([...this.posts]);
@@ -34,9 +35,15 @@ export class PostsService {
     return this.postsUpdate.asObservable();
   }
 
+  getPost(id: string) {
+    return this.http.
+    get<{ _id: string; title: string; content: string }>(this.url + id);
+  }
+
   addPost(title: string, content: string) {
     const post: Post = {id: null, title, content};
-    this.http.post<{ message: string, postId: string }>(this.url, post)
+    this.http
+      .post<{ message: string, postId: string }>(this.url, post)
       .subscribe(res => {
         post.id = res.postId;
         this.posts.push(post);
@@ -45,7 +52,8 @@ export class PostsService {
   }
 
   deletePost(postId: string) {
-    this.http.delete(this.url + postId)
+    this.http
+      .delete(this.url + postId)
       .subscribe(() => {
         this.posts = this.posts.filter(post => post.id !== postId);
         this.postsUpdate.next([...this.posts]);
