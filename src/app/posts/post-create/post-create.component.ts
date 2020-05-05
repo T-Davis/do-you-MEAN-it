47950661dd174d43c3f7b8e895c752fa2cdd66e1
Trevor Davis <tzendou@gmail.com>
2020-05-05 10:07:12 -0700

    Add routing for '/edit/:postId'

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,18 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
 import {PostsService} from '../posts.service';
+import {Post} from '../post';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
+  post: Post;
+  private isNewPost = true;
 
-  constructor(public postsService: PostsService) {
+  constructor(public postsService: PostsService, public route: ActivatedRoute) {
   }
 
-  onAddPost(form: NgForm) {
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap) => {
+      if (paramMap.has('postId')) {
+        this.isNewPost = false;
+        this.post = this.postsService.getLocalPost(paramMap.get('postId'));
+      }
+    });
+  }
+
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
