11976f7dced8ca4e0fb65f662479cc37b7784909
Trevor Davis <tzendou@gmail.com>
2020-05-04 18:11:42 -0700

    Add PUT http method

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS");
+    "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
@@ -43,6 +43,20 @@ app.post("/api/posts", (req, res) => {
     });
   });
 });
+
+app.put("/api/posts/:id", (req, res) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({_id: req.params.id}, post).then(result => {
+    console.log(result);
+    res.status(200).json({
+      message: "Post updated successfully"
+    })
+  })
+})
 
 app.get("/api/posts", (req, res) => {
   Post.find().then(documents => {
