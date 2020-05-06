4864ea48b42d2acf9c35e8f7b4b09630613a6ad3
Trevor Davis <tzendou@gmail.com>
2020-05-06 14:20:00 -0700

    Add a loading-spinner for when the post-list is updating

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -11,15 +11,19 @@ import {Subscription} from 'rxjs';
 export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   private postsSub = Subscription.EMPTY;
+  isLoading = false;
 
   constructor(public postsService: PostsService) {
   }
 
   ngOnInit() {
+    this.isLoading = true;
     this.postsService.getPosts();
-    this.postsSub = this.postsService.getPostsUpdateListener().subscribe((posts: Post[]) => {
-      this.posts = posts;
-    });
+    this.postsSub = this.postsService.getPostsUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.isLoading = false;
+        this.posts = posts;
+      });
   }
 
   onDeletePost(postId: string) {
