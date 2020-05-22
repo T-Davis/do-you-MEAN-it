bb8ccd8cfc97a50e4d9cee40122c33fc2576624e
Trevor Davis <tzendou@gmail.com>
2020-05-22 14:46:51 -0700

    Add functionality to file picker button

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -24,6 +24,9 @@ export class PostCreateComponent implements OnInit {
       }),
       content: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)]
+      }),
+      image: new FormControl(null, {
+        validators: [Validators.required]
       })
     });
     this.route.paramMap.subscribe((paramMap) => {
@@ -35,6 +38,12 @@ export class PostCreateComponent implements OnInit {
         });
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
   }
 
   onSavePost() {
