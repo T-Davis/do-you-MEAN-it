51cf4b5b1002c0dba7adbf354ac85ba96c901fc8
Trevor Davis <tzendou@gmail.com>
2020-06-10 19:10:59 -0700

    Add onSignup() method

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+import {AuthService} from '../auth.service';
 
 @Component({
   templateUrl: './signup.component.html',
@@ -7,4 +10,14 @@ import {Component} from '@angular/core';
 export class SignupComponent {
   isLoading = false;
 
+  constructor(public authService: AuthService) {
+  }
+
+  onSignup(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    this.authService.createUser(form.value.email, form.value.password);
+  }
 }
