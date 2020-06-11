78f987f8fe01a4b283917439271cf00d3a0592ba
Trevor Davis <tzendou@gmail.com>
2020-06-10 19:10:17 -0700

    Add onLogin() method

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+import {AuthService} from '../auth.service';
 
 @Component({
   templateUrl: './login.component.html',
@@ -6,4 +9,15 @@ import {Component} from '@angular/core';
 })
 export class LoginComponent {
   isLoading = false;
+
+  constructor(public authService: AuthService) {
+  }
+
+  onLogin(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    this.authService.login(form.value.email, form.value.password);
+  }
 }
